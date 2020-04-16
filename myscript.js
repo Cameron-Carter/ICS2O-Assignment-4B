aecd32e9aaf0c09f3e876df81a10cc0891ce06dc
Ubuntu <ubuntu@ip-172-31-84-110.ec2.internal>
2020-04-16 17:06:29 +0000

    done

--- myscript.js
+++ myscript.js
@@ -5,11 +5,18 @@ let circleRadius;
 let circleArea;
 let circleCircumference;
 const pi = 3.14;
+// function to take inputted value of diameter and calculate area and circumference with it
 function myMath () {
+  //finds inputted value
   circleDiameter = document.getElementById("diameterinput").value;
+  // r = d / 2
   circleRadius = circleDiameter / 2;
+  // A = pi*r^2
   circleArea = pi * circleRadius * circleRadius;
+  // C = pi*d
   circleCircumference = pi * circleDiameter;
+  // alert that outputs the area and circumference with a message
   alert("The area of the circle is " + circleArea + " and the circumference is " + circleCircumference + ".");
 }
+//event listener waiting for double click to trigger function
 window.addEventListener("dblclick", myMath);
